42897e4e9694fe886b4bc0e084f2a79cbd617e28
Amit Kandar <kandaramit2001@gmail.com>
2023-08-19 17:46:07 +0530

    Add microsoft clarity tracking code and replace localhost with live domain

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_ENDPOINT = 'https://api.placefy.in/api/v1/user/getNotified';
-
+const API_ENDPOINT = "https://app.placefy.in/api/v1/user/getNotified";
 export const sendNotification = async (email) => {
     try {
         const response = await axios.post(API_ENDPOINT, { email });
